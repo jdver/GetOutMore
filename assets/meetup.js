fc49bf04786e39c8f7d5bd76bc0cbd5a48950c2e
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-04-19 23:08:49 -0700

    added xhr functionality to the js, did not really change the html file except to remove a comment, and was testing some bootstrap form stuff but undid what i tried, also moved Jose's api key to the top of the js file with mine, and moved the queryURL to the top of the click event

--- assets/meetup.js
+++ assets/meetup.js
@@ -1,29 +1,15 @@
 // JS for Get Out More - Project 1
 'use strict'
 // weather api
-// This is Kris's API key for the weather
+// This is Kris's API key for the weather (OpenWeatherMap)
 const APIKEY = "e9cf524d552ebbdf618ed3daa5819d90";
+// This is Jose's API key for Google API
+const APIKEY2 = "AIzaSyDwS9kORdJagydPY9p50u2rkQdlt46d1WM"
 
-// these are the variables that will hold our user input data for city and state
-// remove the default text values for these userCity and userState variables once our submit button works
-// i have written what i think will grab the right values from our input fields, they're just commented out for now
 
-/* a button needs to be created in order to be pressed for submission (for a click event)
-so that userCity and userState variables will be populated with the values of their input fields
-*/
 var weatherDescription = "";
 var weatherIcon = null;
 var temperature = null;
-
-// Here we are building the URL we need to query the database
-
-// this was the example url from a class activity, the location was just manually in the url (no variable)
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-//   "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
-
-// here is the url we would use for current weather data for a given city (using a variable for the location)
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherLocation +
-// "&units=imperial&appid=" + APIKey;
 
 document.getElementById("searchButton").addEventListener("click", function(event) {
     // we prevent the default of refreshing the page when the form button is pressed  
@@ -33,99 +19,189 @@ document.getElementById("searchButton").addEventListener("click", function(event
     var userState = document.getElementById("inputState").value;
     var weatherLocation = `${userCity},${userState}`;
 
-    // here is the url we would use for a forecast for a given city (gives weather in 3 hour intervals)
+    // here is the url for requesting a forecast for a given city (gives weather in 3-hour intervals)
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + weatherLocation +
     "&units=imperial&appid=" + APIKEY;
+    // here is the url for requesting events for a given city
+    var queryURL2 = "https://www.googleapis.com/customsearch/v1?key=" + APIKEY2 +
+        "&cx=017456614146260198716:zjzb0jps8te&q=" + userCity + "+" + userState + "+events";
 
     // Here we run our AJAX call to the OpenWeatherMap API
-    // this needs to be set up with a click event with our submit button
-    //
-    // the fetch request needs to have an if statement about if the browser supports fetch
-    fetch(queryURL, {
+    if (window.fetch) {
+        fetch(queryURL, {
+            method: "GET"
+            })
+            .then((result) => result.json()) // make our response object a json object of the results
+            .then((response) => { // accept the result.json() returned from the previous callback as response
+                // Log the queryURL
+                console.log(queryURL);
+
+                // Log the resulting object
+                console.log(response);
+
+                // setting up an initial 5-day forecast div to display before the weather data
+                var forecastIntro = document.createElement("div");
+                forecastIntro.setAttribute("class", "col-2");
+                forecastIntro.innerHTML = `<p>5-day weather forecast for ${userCity}, ${userState}</p>`;
+                document.getElementById("weatherDisplay").appendChild(forecastIntro);
+
+                // displaying the day of the week along with the weather
+                var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+                var dayNum = null;
+                var dayOfWeek = days[dayNum];
+
+                // segments of the 5-day forecast, each 24 hours apart
+                let weatherForecasts = [3, 11, 19, 27, 35];
+
+                // loop through our newly added train data and add it into our schedule table
+                for (let segment of weatherForecasts) {
+                    let newWeatherColumn = document.createElement("div");
+                    newWeatherColumn.setAttribute("class", "col-2");
+                    // converting the unix date "dt" value to the day of the week 
+                    dayNum = new Date(response.list[segment].dt * 1000).getDay();
+                    dayOfWeek = days[dayNum];
+                    // grabbing the weather icon code, temperature (rounded to the nearest integer), and weather description
+                    // pulled weather icons from: https://openweathermap.org/weather-conditions to use in img tags
+                    weatherIcon = response.list[segment].weather[0].icon;
+                    temperature = Math.round(response.list[segment].main.temp);
+                    weatherDescription = response.list[segment].weather[0].description;
+                    // piecing the weather data together to be displayed in a column for each day of the forecast
+                    newWeatherColumn.innerHTML = `<p>${dayOfWeek}</p><p><img src=
+                    "https://openweathermap.org/img/w/${weatherIcon}.png"> ${temperature}°F ${weatherDescription}</p>`;
+                    document.getElementById("weatherDisplay").appendChild(newWeatherColumn);
+                }
+
+                // hide the city & state inputs & the original search button when we display these results
+                // this is accomplished by hiding the entire row that contains them (added an id to it)          
+                document.getElementById("homepageSearchRow").setAttribute("style", "display: none");
+                // and displaying the New Search button
+                document.getElementById("newSearchButton").removeAttribute("style");
+
+                
+            });
+    
+    
+
+        
+
+        // Here we run our AJAX call to the Google API
+        fetch(queryURL2, {
         method: "GET"
         })
         .then((result) => result.json()) // make our response object a json object of the results
         .then((response) => { // accept the result.json() returned from the previous callback as response
-            // Log the queryURL
-            console.log(queryURL);
+        // Log the queryURL
+        console.log(queryURL2);
 
-            // Log the resulting object
-            console.log(response);
+        // Log the resulting object
+        console.log(response);
 
-            // setting up an initial 5-day forecast div to display before the weather data
-            var forecastIntro = document.createElement("div");
-            forecastIntro.setAttribute("class", "col-2");
-            forecastIntro.innerHTML = `<p>5-day weather forecast for ${userCity}, ${userState}</p>`;
-            document.getElementById("weatherDisplay").appendChild(forecastIntro);
-
-            // displaying the day of the week along with the weather
-            var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
-            var dayNum = null;
-            var dayOfWeek = days[dayNum];
-
-            // these are the segments of the 5-day forecast that are for noon each day
-            let weatherForecasts = [3, 11, 19, 27, 35];
-
-            // loop through our newly added train data and add it into our schedule table
-            for (let segment of weatherForecasts) {
-                let newWeatherColumn = document.createElement("div");
-                newWeatherColumn.setAttribute("class", "col-2");
-                // converting the unix date "dt" value to the day of the week 
-                dayNum = new Date(response.list[segment].dt * 1000).getDay();
-                dayOfWeek = days[dayNum];
-                // grabbing the weather icon code, temperature (rounded to the nearest integer), and weather description
-                // pulled weather icons from: https://openweathermap.org/weather-conditions to use in img tags
-                weatherIcon = response.list[segment].weather[0].icon;
-                temperature = Math.round(response.list[segment].main.temp);
-                weatherDescription = response.list[segment].weather[0].description;
-                // piecing the weather data together to be displayed in a column for each day of the forecast
-                newWeatherColumn.innerHTML = `<p>${dayOfWeek}</p><p><img src=
-                "https://openweathermap.org/img/w/${weatherIcon}.png"> ${temperature}°F ${weatherDescription}</p>`;
-                document.getElementById("weatherDisplay").appendChild(newWeatherColumn);
-            }
-
-            // hide the city & state inputs & the original search button when we display these results
-            // this is accomplished by hiding the entire row that contains them (added an id to it)          
-            document.getElementById("homepageSearchRow").setAttribute("style", "display: none");
-            // and displaying the New Search button
-            document.getElementById("newSearchButton").removeAttribute("style");
-
-            
+        for (let i = 0; i < 5; i++) {
+            let newEventColumn = document.createElement("div");
+            newEventColumn.setAttribute("class", "col-2");
+            var eventImage = response.items[i].pagemap.cse_thumbnail[0].src;
+            console.log(eventImage);
+            var eventDisplay = document.createElement("div");
+            eventDisplay.innerHTML = `<img src="${eventImage}" alt="display">`;
+            document.getElementById("eventsDisplay").appendChild(eventDisplay);
+        }
+        
         });
-  
-    const APIKEY2 = "AIzaSyDwS9kORdJagydPY9p50u2rkQdlt46d1WM"
-    var queryURL2 = "https://www.googleapis.com/customsearch/v1?key=" + APIKEY2 +
-    "&cx=017456614146260198716:zjzb0jps8te&q=" + userCity + "+" + userState + "+events";
+    
+    } else {
+        // create new xhr object for the weather API
+        const xhr = new XMLHttpRequest();
+        // set an AJAX GET request using our queryURL
+        xhr.open("GET", queryURL);
+        // once the xhr loads, we want to...
+        xhr.onload = event => {
+            // check if the request to the weather API has finished...
+            if (xhr.readyState === 4) {
+                // check of the request has successfully retrieved data from the weather API
+                if (xhr.status === 200) {
+                    // parse our response into json so we can grab parts of it more easily
+                    let response = JSON.parse(xhr.responseText);
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    // this needs to be set up with a click event
-    fetch(queryURL2, {
-    method: "GET"
-    })
-    .then((result) => result.json()) // make our response object a json object of the results
-    .then((response) => { // accept the result.json() returned from the previous callback as response
-    // Log the queryURL
-    console.log(queryURL2);
+                    console.log(response);
 
-    // Log the resulting object
-    console.log(response);
+                    // setting up an initial 5-day forecast div to display before the weather data
+                    var forecastIntro = document.createElement("div");
+                    forecastIntro.setAttribute("class", "col-2");
+                    forecastIntro.innerHTML = `<p>5-day weather forecast for ${userCity}, ${userState}</p>`;
+                    document.getElementById("weatherDisplay").appendChild(forecastIntro);
 
-      for (let i = 0; i < 5; i++) {
-          let newEventColumn = document.createElement("div");
-          newEventColumn.setAttribute("class", "col-2");
-          var eventImage = response.items[i].pagemap.cse_thumbnail[0].src;
-          console.log(eventImage);
-          var eventDisplay = document.createElement("div");
-          eventDisplay.innerHTML = `<img src="${eventImage}" alt="display">`;
-          document.getElementById("eventsDisplay").appendChild(eventDisplay);
-      }
-      
-    });
-    // need to add an XHR request in case fetch is not supported
-    /* XHR request needs to be tested by setting the if statement for the fetch request to "not supported"
-    so it will default to the XHR request instead. this can be verified with a console.log within the
-    XHR request, e.g. "XHR is running."
-    */
+                    // displaying the day of the week along with the weather
+                    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+                    var dayNum = null;
+                    var dayOfWeek = days[dayNum];
+
+                    // segments of the 5-day forecast, each 24 hours apart
+                    let weatherForecasts = [3, 11, 19, 27, 35];
+
+                    // loop through our newly added train data and add it into our schedule table
+                    for (let segment of weatherForecasts) {
+                        let newWeatherColumn = document.createElement("div");
+                        newWeatherColumn.setAttribute("class", "col-2");
+                        // converting the unix date "dt" value to the day of the week 
+                        dayNum = new Date(response.list[segment].dt * 1000).getDay();
+                        dayOfWeek = days[dayNum];
+                        // grabbing the weather icon code, temperature (rounded to the nearest integer), and weather description
+                        // pulled weather icons from: https://openweathermap.org/weather-conditions to use in img tags
+                        weatherIcon = response.list[segment].weather[0].icon;
+                        temperature = Math.round(response.list[segment].main.temp);
+                        weatherDescription = response.list[segment].weather[0].description;
+                        // piecing the weather data together to be displayed in a column for each day of the forecast
+                        newWeatherColumn.innerHTML = `<p>${dayOfWeek}</p><p><img src=
+                        "https://openweathermap.org/img/w/${weatherIcon}.png"> ${temperature}°F ${weatherDescription}</p>`;
+                        document.getElementById("weatherDisplay").appendChild(newWeatherColumn);
+                }
+
+                
+                }
+            }
+        }
+        // send the request to the weather API
+        xhr.send();
+
+        // create new xhr object for the Google API
+        const xhr2 = new XMLHttpRequest();
+        // set an AJAX GET request using our queryURL
+        xhr2.open("GET", queryURL2);
+        // once the xhr loads, we want to...
+        xhr2.onload = event => {
+            // check if the request to the Google API has finished...
+            if (xhr2.readyState === 4) {
+                // check of the request has successfully retrieved data from the Google API
+                if (xhr2.status === 200) {
+                    // parse our response into json so we can grab parts of it more easily
+                    let response = JSON.parse(xhr2.responseText);
+
+                    console.log(response);
+
+                    for (let i = 0; i < 5; i++) {
+                        let newEventColumn = document.createElement("div");
+                        newEventColumn.setAttribute("class", "col-2");
+                        var eventImage = response.items[i].pagemap.cse_thumbnail[0].src;
+                        console.log(eventImage);
+                        var eventDisplay = document.createElement("div");
+                        eventDisplay.innerHTML = `<img src="${eventImage}" alt="display">`;
+                        document.getElementById("eventsDisplay").appendChild(eventDisplay);
+                    }
+                }
+            }
+        }
+        // send the request to the Google API
+        xhr2.send();
+
+
+        // hide the city & state inputs & the original search button when we display these results
+                // this is accomplished by hiding the entire row that contains them (added an id to it)          
+                document.getElementById("homepageSearchRow").setAttribute("style", "display: none");
+                // and displaying the New Search button
+                document.getElementById("newSearchButton").removeAttribute("style");
+
+    }
+    
 });
 
 // click event for New Search button
